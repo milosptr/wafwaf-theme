f84fb7ef0a0db1743091abe7405622667923d6a8
milospetrovic <milosp@ultimatemovers.net>
2021-07-22 19:29:53 +0200

    finish up menu, footer, homepage

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 (function() {
+  const rellax = new Rellax('.rellax')
   const swiper = new Swiper(".custom-carousel", {
     cssMode: true,
+    loop: true,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
     pagination: {
       el: ".swiper-pagination",
+      clickable: true
     },
     mousewheel: true,
     keyboard: true,
-  });
+  })
 })()
