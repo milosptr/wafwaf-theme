8227bd93ad8874ef636f33cb296728582adf904c
milospetrovic <milosp@ultimatemovers.net>
2021-07-24 17:03:43 +0200

    add fonts and menu page

--- js/main.js
+++ js/main.js
@@ -8,15 +8,27 @@
     easing: 'cubic-bezier(0.5, 0, 0, 1)',
   };
 
-  document.querySelector('.showcase-group').childNodes.forEach((c, i) => {
-    if(c && c.classList && c.classList.contains('fadeInUp')) {
-      slideUp.delay = c.dataset['delay'] ? parseInt(c.dataset['delay']) : 275
-      ScrollReveal().reveal(c, slideUp);
-    }
-  })
+  if(document.querySelector('.showcase-group'))
+    document.querySelector('.showcase-group').childNodes.forEach((c, i) => {
+      if(c && c.classList && c.classList.contains('fadeInUp')) {
+        slideUp.delay = c.dataset['delay'] ? parseInt(c.dataset['delay']) : 275
+        ScrollReveal().reveal(c, slideUp);
+      }
+    })
 
   ScrollReveal().reveal('.slide-up', slideUp);
 
+  if(document.body.id === 'menu') {
+    const categories = document.querySelectorAll('.category-title')
+    categories[0].classList.add('active')
+    filterProducts(categories[0].innerText)
+
+    categories.forEach((c) => {
+      c.addEventListener('click', (e) => {
+        filterProducts(e.target.innerText)
+      })
+    })
+  }
 
   const swiper = new Swiper(".custom-carousel", {
     cssMode: true,
@@ -41,4 +53,21 @@
   // })
   //   .then(response => response.json())
   //   .then(data => console.log(data))
+
+  function filterProducts(category) {
+    const categories = document.querySelectorAll('.category-title')
+    const products = [...document.querySelector('.products-container').children]
+    products.forEach((p) => {
+      p.style = ''
+      p.classList.remove('hide')
+      p.style.opacity = 1
+      if(p.dataset.category !== category)
+        p.classList.add('hide')
+    })
+    categories.forEach((c) => {
+      c.classList.remove('active')
+      if(c.innerText === category)
+        c.classList.add('active')
+    })
+  }
 })()
--- menu.md
+++ menu.md
@@ -8,6 +8,10 @@ categories:
     product_title: EGG WAFF
     product_description: egg, bacon, asparagus, souce
     product_price: 160
+  - product_image: "/uploads/2021/07/20/mask-group-133.png"
+    product_title: EGG WAFF
+    product_description: egg, bacon, asparagus, souce
+    product_price: 160
 - category_title: SLADKÉ
   products:
   - product_image: "/uploads/2021/07/24/mask-group-27.png"
@@ -18,5 +22,12 @@ categories:
     product_image: "/uploads/2021/07/24/mask-group-26.png"
     product_title: WAFTELLA
     product_description: nutella, oreo, jahody, maliny, mascarpone
-
+  - product_image: "/uploads/2021/07/24/mask-group-27.png"
+    product_title: WAFREO
+    product_description: jahody, nutella, oreo, domácí šlehačka
+    product_price: 112
+  - product_price: 134
+    product_image: "/uploads/2021/07/24/mask-group-26.png"
+    product_title: WAFTELLA
+    product_description: nutella, oreo, jahody, maliny, mascarpone
 ---
